13466de38df11f5576ab9f8a18a297327c93bfbd
Manny <Manuelreyes1241@Outlook.com>
2024-08-14 02:37:27 -0400

    Routes:creates routes for employee and tasks
    - Route to get edit employees
    - Route to get edit task

--- routes/employeesRoutes.js
+++ routes/employeesRoutes.js
@@ -7,7 +7,22 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const employees = await Employee.findAll({include: [Task]});
-    res.json(employees);
+    res.status(200).json(employees);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Update an employee by ID
+router.put('/:id', async (req, res, next) => {
+  try {
+    const employee = await Employee.findByPk(req.params.id);
+    if (employee) {
+      const updatedEmployee = await employee.update(req.body);
+      res.status(200).json(updatedEmployee);
+    } else {
+      res.status(404).send('Employee not found');
+    }
   } catch (err) {
     next(err);
   }
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -6,8 +6,23 @@ const router = express.Router();
 // Get all tasks
 router.get('/', async (req, res, next) => {
   try {
-    const tasks = await Task.findAll({include: [Employee]});
-    res.json(tasks);
+    const tasks = await Task.findAll({ include: [Employee] });
+    res.status(200).json(tasks);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Update a task by ID
+router.put('/:id', async (req, res, next) => {
+  try {
+    const task = await Task.findByPk(req.params.id);
+    if (task) {
+      const updatedTask = await task.update(req.body);
+      res.status(200).json(updatedTask);
+    } else {
+      res.status(404).send('Task not found');
+    }
   } catch (err) {
     next(err);
   }
